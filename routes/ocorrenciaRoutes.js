c56e954a9367608f43938e8f132a8c5913369534
Rayssa <rvms@discente.ifpe.edu.br>
2025-12-02 11:12:41 -0300

    2

--- routes/ocorrenciaRoutes.js
+++ routes/ocorrenciaRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const OcorrenciaController = require("../controllers/ocorrenciaController");
 const auth = require("../middlewares/usuarioAuth")
 
-router.get("/ocorrencias/cadastrar",auth, OcorrenciaController.cadastrar);
+router.get("/ocorrencias/cadastrar", OcorrenciaController.cadastrar);
 router.get("/ocorrencias",auth, OcorrenciaController.relatorio);
-router.post("/ocorrencias",auth, OcorrenciaController.salvar);
-router.post("/ocorrencias",auth, OcorrenciaController.salvar);
+router.post("/ocorrencias", OcorrenciaController.salvar);
+router.post("/ocorrencias", OcorrenciaController.salvar);
 router.get("/ocorrencias/:numero",auth, OcorrenciaController.detalhar);
 router.get("/ocorrencias/:numero/remover",auth, OcorrenciaController.remover);
 router.get("/ocorrencias/:numero/atualizar",auth, OcorrenciaController.atualizar);
